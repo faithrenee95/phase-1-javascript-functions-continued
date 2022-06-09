f3226356f720b617e488e28b814699f6a1bcb0d8
faith <faithreneemartin@gmail.com>
2022-06-09 18:32:09 -0400

    commit tree 9a58f8c337600d29a3b23e86f5bc32850fad53bd

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate', ){
+    return (`This Saturday, I want to ${activity}!`)
+   
+}
+
+const mondayWork = function() {
+    console.log("go to the office");
+  }
